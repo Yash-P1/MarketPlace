3fb8fc01c9ecc7e748df730a7fcfa7b5e36425ee
Yash Patel <yspatel97@gmail.com>
2022-12-19 19:55:59 -0500

    readme changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,6 @@ function App() {
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
-            {/* <Route path="/contact" element={<Contact />} /> */}
             <Route path="/cart" element={<Cart />} />
           </Routes>
         </Router>
